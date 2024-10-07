2a4d0052bbf9e75b66f19d6dc9cebe5f4814f615
muthuramalingamdurai <ramdurai25@gmail.com>
2024-10-07 15:32:56 +0530

    new code
    new code

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/user', (req, res) => {
+    let userList = [{name:"Muthu",role:"admin"},{name:"Ram",role:"user"}]
+   return res.json({status:200,msg:"Success",data:userList})
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
