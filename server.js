690c5461181aadb7d5a59f816f6d3c706aa2c5b9
muthuramalingamd <ramdurai25@gmail.com>
2024-10-08 12:44:07 +0530

    ui code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 const port = 4200;
 
@@ -9,6 +9,7 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/user', (req, res) => {
     let userList = [{name:"Muthu",role:"admin"},{name:"Ram",role:"user"}]
